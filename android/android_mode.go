9b893fb3e08a00f0b8b2e0746f8e74103c6fb946
wailovet <wailovet@163.com>
2022-08-01 16:52:01 +0800

    fix

--- android/android_mode.go
+++ android/android_mode.go
@@ -2,9 +2,17 @@ package android
 
 import (
 	"fmt"
+	"image/color"
 	"time"
 
 	"gioui.org/app"
+	_ "gioui.org/app/permission/networkstate"
+	"gioui.org/font/gofont"
+	"gioui.org/io/system"
+	"gioui.org/layout"
+	"gioui.org/op"
+	"gioui.org/text"
+	"gioui.org/widget/material"
 	"github.com/inkeliz/gowebview"
 	"github.com/wailovet/gofunc"
 	"github.com/wailovet/nuwa"
@@ -45,22 +53,70 @@ func Run(title string, hes ...*nuwa.HttpEngine) {
 
 func loop(w *app.Window, title string, port int) error {
 
-	var wv gowebview.WebView
+	th := material.NewTheme(gofont.Collection())
+	var ops op.Ops
+
+	var (
+		config  = &gowebview.Config{URL: fmt.Sprint("http://127.0.0.1:", port), WindowConfig: &gowebview.WindowConfig{Title: title, VM: app.JavaVM()}}
+		webview gowebview.WebView
+	)
+
 	for {
 		e := <-w.Events()
 		switch e := e.(type) {
 		case app.ViewEvent:
-			if wv == nil {
+			//----------------------------------
+			config.WindowConfig.Window = e.View // Here, sets the GioView. ;)
+			//----------------------
+			if webview == nil {
 				go func() {
 					var err error
-					wv, err = gowebview.New(&gowebview.Config{URL: fmt.Sprint("http://127.0.0.1:", port), WindowConfig: &gowebview.WindowConfig{Title: title, Window: e.View, VM: app.JavaVM()}})
+					webview, err = gowebview.New(config)
 					if err != nil {
 						panic(err)
 					}
-					defer wv.Destroy()
-					wv.Run()
+					defer webview.Destroy()
+					webview.Run()
 				}()
+			} else {
+				webview.SetVisibility(gowebview.VisibilityMaximized)
 			}
+		case *system.CommandEvent:
+			// You can minimize when click on "Back", without destroy.
+			// It can be used when the webview is open in response to a button-click.
+			if e.Type == system.CommandBack {
+				e.Cancel = true
+				webview.SetVisibility(gowebview.VisibilityMinimized)
+			}
+		case system.DestroyEvent:
+			return e.Err
+		case system.FrameEvent:
+			gtx := layout.NewContext(&ops, e)
+			l := material.H1(th, "Hello, Gio")
+			maroon := color.NRGBA{R: 127, G: 0, B: 0, A: 255}
+			l.Color = maroon
+			l.Alignment = text.Middle
+			l.Layout(gtx)
+			e.Frame(gtx.Ops)
 		}
 	}
+
+	// var wv gowebview.WebView
+	// for {
+	// 	e := <-w.Events()
+	// 	switch e := e.(type) {
+	// 	case app.ViewEvent:
+	// 		if wv == nil {
+	// 			go func() {
+	// 				var err error
+	// 				wv, err = gowebview.New(&gowebview.Config{URL: fmt.Sprint("http://127.0.0.1:", port), WindowConfig: &gowebview.WindowConfig{Title: title, Window: e.View, VM: app.JavaVM()}})
+	// 				if err != nil {
+	// 					panic(err)
+	// 				}
+	// 				defer wv.Destroy()
+	// 				wv.Run()
+	// 			}()
+	// 		}
+	// 	}
+	// }
 }
--- example/go.mod
+++ example/go.mod
@@ -4,5 +4,5 @@ go 1.16
 
 require (
 	github.com/wailovet/nuwa v0.0.0-20220630140825-046b54961409
-	github.com/wailovet/nuwa-android v0.0.0-20220801060005-48a6141d9690
+	github.com/wailovet/nuwa-android master
 )
